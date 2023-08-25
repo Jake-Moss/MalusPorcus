9d826f95fdc3c59ea775f93cf9b6c849920cdde6
Tom Stephen <tom.stephen@icloud.com>
2023-08-26 06:14:36 +1000

    stuff

--- main.c
+++ main.c
@@ -6,13 +6,41 @@ int main(void) {
 
     InitWindow(screenWidth, screenHeight, "Cozy Desk Simulator");
 
+    // TODO: force aspect ratio
+
+    // SetWindowOpacity(0.8); 
+
+    SetTargetFPS(60);
+
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
+
+    RenderTexture2D renderTexture = LoadRenderTexture(screenWidth, screenHeight);
+
     while (!WindowShouldClose()) {
-        BeginDrawing();
+        // typically things are done between `BeginDrawing()` and `EndDrawing()`.
+        // Instead, we're going to draw to a texture, so our actual drawing is just
+        // drawing the texture itself. Allows for scaling for pixel-accurate
+        // resizing/fullscreen.
+        BeginTextureMode(renderTexture);
             ClearBackground(RAYWHITE);
 
             DrawText("WELCOME", screenWidth / 2, screenHeight / 2, 20, LIGHTGRAY);
+            DrawText("For the dawgs", screenWidth / 2, screenHeight / 2 - 50, 20, LIGHTGRAY);
+        EndTextureMode();
+
+        BeginDrawing(); // nobody touch this! Do your drawing in the BeginTextureMode above
+            DrawTexturePro(
+                renderTexture.texture,
+                (Rectangle){ 0, 0, (float)(renderTexture.texture.width), (float)(-renderTexture.texture.height) },
+                (Rectangle){ 0, 0, (float)(GetScreenWidth()), (float)(GetScreenHeight()) },
+                (Vector2){ 0, 0 },
+                0,
+                WHITE
+            );
         EndDrawing();
     }
+
+    UnloadRenderTexture(renderTexture);
 
     CloseWindow();
 
